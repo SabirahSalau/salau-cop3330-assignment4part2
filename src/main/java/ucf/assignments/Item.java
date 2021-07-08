bdb4dd458afdab51d8fa3dbd37e67aeaee46479a
Sabirah Salau <salausabirah@gmail.com>
2021-07-08 16:04:48 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/Item.java
@@ -0,0 +1,9 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Sabirah Salau
+ */
+
+package ucf.assignments;
+
+public class Item {
+}
---
+++ src/test/java/ucf/assignments/ListManagerTest.java
@@ -0,0 +1,72 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Sabirah Salau
+ */
+
+
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListManagerTest {
+
+    @Test
+    void renameList() {
+        //Change list name
+        //Check to see if old list name is present
+        //Should return false for successful test.
+    }
+
+    @Test
+    void deleteList() {
+        //Delete list
+        //Check to see if old list name is present
+        //Should return false for successful test.
+    }
+
+    @Test
+    void addList() {
+        //Check to see if number of lists increases
+        //Check to see if the name of the new list is in the list of lists
+    }
+
+    @Test
+    void saveList() {
+        //Get list title and item info
+        //Save to txt file
+    }
+
+    @Test
+    void saveAll() {
+        //Save all lists to txt file
+        //Check to see if file list names correspond with list names
+    }
+
+    @Test
+    void addItem() {
+        //Check to see if number of items increases
+        //Check to see if the name of the new item is in the list of items
+    }
+
+    @Test
+    void deleteItem() {
+        //Delete item
+        //Check to see if old item name is present
+        //Should return false for successful test.
+    }
+
+    @Test
+    void isComplete(){
+        //Check item box as complete
+        //If value is true, test successful
+    }
+
+    @Test
+    void editItem(){
+        //Accept new values for description and due date
+        //Assign values and check changes
+    }
+
+}
