2edea794ae54d32e923acaa8871a48b46ca52672
Sabirah Salau <salausabirah@gmail.com>
2021-07-11 21:02:14 -0400

    Progress2.0

--- src/main/java/ucf/assignments/Item.java
+++ src/main/java/ucf/assignments/Item.java
@@ -5,5 +5,36 @@
 
 package ucf.assignments;
 
+import javafx.scene.control.CheckBox;
+
+import java.time.LocalDate;
+
 public class Item {
+
+    private String description;
+    private LocalDate date;
+
+
+
+    public Item(String description, LocalDate date) {
+        this.description = description;
+        this.date = date;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
